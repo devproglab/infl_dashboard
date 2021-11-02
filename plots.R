d2e2506381f27e6f98ce01db2f5257aa0d72d44b
devproglab <iluagulenkov@gmail.com>
2021-11-02 19:28:40 +0300

    remove install.packages

--- plots.R
+++ plots.R
@@ -2,11 +2,11 @@
 using <- function(...) {
   libs <- unlist(list(...))
   req <- unlist(lapply(libs,require,character.only=TRUE))
-  need <- libs[req==FALSE]
-  if (length(need)>0) { 
-    install.packages(need)
-    lapply(need,require,character.only=TRUE)
-  }
+  # need <- libs[req==FALSE]
+  # if (length(need)>0) {
+  #   install.packages(need)
+  #   lapply(need,require,character.only=TRUE)
+  # }
 }
 
 using("data.table", "readxl", "dplyr", "xts", "plotly", "stats", "writexl",
